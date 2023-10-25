30dc2e6ffb2f1d7cd55143cb4a5f2d3323b03be1
rdill-83 <57857105+rdill-83@users.noreply.github.com>
2023-10-25 13:09:23 -0700

    23-10-25 Commit

--- _GitHub-Actions_Notes.sh
+++ _GitHub-Actions_Notes.sh
@@ -10,3 +10,20 @@ az group create --name $RG --location $loc
 
 $groupid =$(az group show --name $RG --query id --output tsv)
 
+# Create Service Principal:
+az ad sp create-for-rbac --scope $groupid --role Contributor --sdk-auth
+
+### service principal creation - JSON output (clientID used in later step)
+{
+  "clientId": "16e3c10b-a8ae-4dd3-b355-f415465deb96",
+  "clientSecret": "Gha8Q~GEvmf_lzTLeunfFm1GmOJ-7egZ5_rZWajv",
+  "subscriptionId": "da4de53d-f7e7-49c8-bbbe-3533fb81ee12",
+  "tenantId": "8ceb2077-8b6c-4385-add7-66e6059a805a",
+  "activeDirectoryEndpointUrl": "https://login.microsoftonline.com",
+  "resourceManagerEndpointUrl": "https://management.azure.com/",
+  "activeDirectoryGraphResourceId": "https://graph.windows.net/",
+  "sqlManagementEndpointUrl": "https://management.core.windows.net:8443/",
+  "galleryEndpointUrl": "https://gallery.azure.com/",
+  "managementEndpointUrl": "https://management.core.windows.net/"
+}
+
