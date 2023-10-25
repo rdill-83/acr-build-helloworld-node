cccf6b68c2658baf0a59a0bbf2d3f53d6527a494
rdill-83 <57857105+rdill-83@users.noreply.github.com>
2023-10-25 13:35:51 -0700

    23-10-25 Commit

--- _GitHub-Actions_Notes.sh
+++ _GitHub-Actions_Notes.sh
@@ -2,9 +2,9 @@
 
 # Source: https://learn.microsoft.com/en-us/azure/container-instances/container-instances-github-action?tabs=userlevel
 
-### AZ Env - General Setup:
+# AZ Env - General Setup:
 
-# Resource Group Creation:
+### Resource Group Creation:
 $RG = 'github-actions'
 $Loc = "EastUS2"
 $ACR = "ghacr90814"
@@ -13,10 +13,10 @@ az group create --name $RG --location $loc
 
 $groupid =$(az group show --name $RG --query id --output tsv)
 
-# Create Service Principal:
+### Create Service Principal:
 az ad sp create-for-rbac --scope $groupid --role Contributor --sdk-auth
 
-### service principal creation - JSON output (clientID used in later step)
+#### service principal creation - JSON output (clientID used in later step)
 {
   "clientId": "16e3c10b-a8ae-4dd3-b355-f415465deb96",
   "clientSecret": "Gha8Q~GEvmf_lzTLeunfFm1GmOJ-7egZ5_rZWajv",
@@ -30,11 +30,14 @@ az ad sp create-for-rbac --scope $groupid --role Contributor --sdk-auth
   "managementEndpointUrl": "https://management.core.windows.net/"
 }
 
-# Bonus - show az sp info (live example):
+#### Bonus - show az sp info (live example):
 az ad sp show --id 16e3c10b-a8ae-4dd3-b355-f415465deb96
 
-# Create AZ Container Registry:
+### Create AZ Container Registry:
 az acr create -g $RG --name $ACR --sku standard --admin-enabled $true
 
-### 
+### ACR ID to Variable:
 $regID = $(az acr show -g $RG --name $ACR --query id --output tsv)
+
+### Add AcrPush role to the SP:
+az role assignment create --assignee 16e3c10b-a8ae-4dd3-b355-f415465deb96 --scope $regID --role AcrPush
